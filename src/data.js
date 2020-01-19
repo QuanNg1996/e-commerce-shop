8cccae84fce3b18fd4b5b66e443aac773e7b1300
Quan Nguyen <quanguyen250@gmail.com>
2020-01-18 22:21:45 -0500

    data update

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const storeProducts = [
     price: 10,
     company: "GOOGLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Google Pixel is powered by a 1.6GHz quad-core Qualcomm Snapdragon 821 processor. It comes with 4GB of RAM. The Google Pixel runs Android 7.1 and is powered by a 2770mAh non-removable battery. As far as the cameras are concerned, the Google Pixel on the rear packs 12.3-megapixel camera.",
     inCart: false,
     count: 0,
     total: 0
@@ -23,7 +23,7 @@ export const storeProducts = [
     price: 16,
     company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The S7, S7 Edge & S7 Active feature a 1440p Quad HD Super AMOLED display; the S7 & S7 Active both have a 5.1-inch panel, while the S7 Edge uses a larger 5.5-inch panel. ... Galaxy S7 devices are equipped with an octa-core Exynos 8890 system on a chip and 4GB of RAM",
     inCart: false,
     count: 0,
     total: 0
@@ -35,7 +35,7 @@ export const storeProducts = [
     price: 8,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light.",
     inCart: false,
     count: 0,
     total: 0
@@ -47,7 +47,7 @@ export const storeProducts = [
     price: 18,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light.",
     inCart: false,
     count: 0,
     total: 0
@@ -59,19 +59,19 @@ export const storeProducts = [
     price: 24,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "HTC Desire 626 comes with a 5.00-inch touchscreen display with a resolution of 720x1280 pixels at a pixel density of 294 pixels per inch (ppi). HTC Desire 626 is powered by a 1.2GHz quad-core Qualcomm Snapdragon 410 processor. It comes with 1GB of RAM.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "Iphone 1",
     img: "img/product-6.png",
     price: 17,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The Apple iPhone 1st Generation combines mobile phone, Internet communications, iPod and other functionalities. The original iPhone introduced a multi-touch interface (control of the functions by dragging finger across the glass display).",
     inCart: false,
     count: 0,
     total: 0
@@ -83,7 +83,7 @@ export const storeProducts = [
     price: 30,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The phone comes with a 4.70-inch touchscreen display with a resolution of 750x1334 pixels at a pixel density of 326 pixels per inch (ppi) and an aspect ratio of 16:9. Apple iPhone 7 is powered by a 2.34GHz quad-core Apple A10 Fusion processor. It comes with 2GB of RAM.",
     inCart: false,
     count: 0,
     total: 0
@@ -95,7 +95,7 @@ export const storeProducts = [
     price: 2,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "A cracked iPhone",
     inCart: false,
     count: 0,
     total: 0
@@ -109,7 +109,7 @@ export const detailProduct = {
   price: 10,
   company: "google",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    "Google Pixel is powered by a 1.6GHz quad-core Qualcomm Snapdragon 821 processor. It comes with 4GB of RAM. The Google Pixel runs Android 7.1 and is powered by a 2770mAh non-removable battery. As far as the cameras are concerned, the Google Pixel on the rear packs 12.3-megapixel camera.",
   inCart: false,
   count: 0,
   total: 0
